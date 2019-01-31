aed4aecd52880b6aa87e8d241561e285550b6a73
hashi <hashimo.169@icloud.com>
2019-01-31 21:39:18 +0900

    modify report edit

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -40,10 +40,11 @@ public class ReportsEditServlet extends HttpServlet {
         em.close();
 
         Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
-        if(login_employee.getId() == r.getEmployee().getId()) {
+        if(r != null) {request.getSession().setAttribute("employee_id", r.getId());
+        //if(login_employee.getId() == r.getEmployee().getId()) {
             request.setAttribute("report", r);
             request.setAttribute("_token", request.getSession().getId());
-            request.getSession().setAttribute("report_id", r.getId());
+            //request.getSession().setAttribute("report_id", r.getId());
         }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
